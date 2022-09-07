6e2380e03c221bd85f125f438735da6616710092
Uk-jake <mag0225@naver.com>
2022-09-07 15:46:43 +0900

    Add 모의고사SourceCode

---
+++ 알고리즘 풀이/모의고사/source/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] inputAnswers = {1, 3, 2, 4, 2};
+
+        //{1, 3, 2, 4, 2}
+
+        System.out.println(sol.solution(inputAnswers)[2]);
+
+
+
+    }
+}
