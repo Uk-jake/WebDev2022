f38f4b5685f2753c9f5d21a1c92b4ef99204991a
Uk-jake <mag0225@naver.com>
2022-09-23 18:33:06 +0900

    Update directory

--- 알고리즘 풀이/폰켓몬/source/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums = {3,3,3,2,2,2};
-
-        System.out.println(solution.solution(nums));
-    }
-}
